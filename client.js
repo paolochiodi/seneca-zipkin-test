242e617c86783142b1fe9c8aa92df313575fa73f
Paolo Chiodi <chiodi84@gmail.com>
2016-09-13 11:35:13 +0200

    Add auto-discovery of actions to override
    Also start to track local actions and expand service2 to make the
    trace tree a bit more complex
    
    This work in two steps:
    - override existing actions at the moment of plugin initialisation
    - override seneca.add so that newly added actions can be overridded at
      runtime
    
    Issues/thinks to check
    - overriding seneca.add is a fragile hack. Need to check it doesn't
      disrupt actions created through plugins etc
    - if a "tracked" action is overridden by the final dev and it uses
      prior, the action will be tracked twice (first time the new action
    will be tracked, second time the "original tracked" one)
    - tracking all actions without sending batch requests to zipkin could
      cause a measurable performance penalty
    - I'm currently filtering seneca internal actions, but probably the
      filter isn't complete

--- client.js
+++ client.js
@@ -6,15 +6,15 @@ var tracer = require('./tracer')
 seneca({
   tag: 'client1'
 })
+.use(tracer)
 .client({
   type:'http',
   port:3000,
   pin: 'a:1'
 })
-.use(tracer, {
-  pins: ['a:1']
+.ready(function () {
+  this.act('a:1', function (err, msg) {
+    console.log(err, msg)
+    setTimeout(process.exit, 2000)
+  });
 })
-.act('a:1', function (err, msg) {
-  console.log(err, msg)
-  setTimeout(process.exit, 2000)
-});
--- service2.js
+++ service2.js
@@ -1,9 +1,11 @@
 var seneca = require('seneca')
+var async = require('async')
 var tracer = require('./tracer')
 
 seneca({
   tag: 'service2'
 })
+.use(tracer)
 .client({
   type:'http',
   port:3002,
@@ -13,12 +15,20 @@ seneca({
   type:'http',
   port:3001
 })
-.add('b:1', function (msg, done) {
-  this.act('c:1', done)
+.add('internal:1', function (msg, done) {
+  var seneca = this
+  async.parallel({
+    result: function (next) { seneca.act('c:1', next) },
+    fake: function (next) { seneca.act('internal:2', next) }
+  }, function (err, res) {
+    done(err,res && res.result)
+  })
 })
-.use(tracer, {
-  pins: [
-    'b:1',
-    'c:1'
-  ]
+.add('internal:2', function (msg, done) {
+  process.nextTick(function () {
+    done()
+  })
+})
+.add('b:1', function (msg, done) {
+  this.act('internal:1', done)
 })
